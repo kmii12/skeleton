e644a9e6c0390c447e6758a2cd07d8a2a156fc00
mk <kanzakimiki.12@gmail.com>
2025-01-07 09:40:00 +0900

    コメントアウトをメモに移動

--- src/firebase.ts
+++ src/firebase.ts
@@ -21,6 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = getFirestore(app);
+// const db = getFirestore(app);
 
-export { db };
+// Firestoreインスタンスの初期化
+export const db = getFirestore(app);
