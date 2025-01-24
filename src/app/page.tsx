8b438205c132683c0dbb0c9fdeb7b9fc57d0f214
mk <kanzakimiki.12@gmail.com>
2025-01-24 15:27:41 +0900

    logo update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.appName}>skeleton</h1>
+      <h1 className={styles.appName}></h1>
       <Link className={styles.btn} href={`/MainCalender`}>
         Go to MyCalender
       </Link>
